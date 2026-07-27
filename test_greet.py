a29b8642554c59631e18ac9f34577b33fa074bd7
unknown <asinaxabdulla@gmail.com>
2026-07-27 13:44:03 +0400

    added testupper case function in test_greet

--- test_greet.py
+++ test_greet.py
@@ -21,7 +21,9 @@ class TestGreet(unittest.TestCase):
         """Test greet with a name containing whitespace."""
         self.assertEqual(greet("   "), "Hello,    !")
 
-    
+    def test_uppercase(self):
+        """Test greet with uppercase conversion."""
+        self.assertEqual(greet("zara"), "Hello, ZARA!")
 
 
 if __name__ == "__main__":
