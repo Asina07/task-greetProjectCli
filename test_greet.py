6d37cca9fffa0520682abb20cbca162b2a75b665
unknown <asinaxabdulla@gmail.com>
2026-07-27 12:21:06 +0400

    update test greet file with added new function test white space name

--- test_greet.py
+++ test_greet.py
@@ -16,6 +16,11 @@ class TestGreet(unittest.TestCase):
     def test_empty_string(self):
         """Test greet with an empty string."""
         self.assertEqual(greet(""), "Hello, !")
+        
+    def test_whitespace_name(self):
+        """Test greet with a name containing whitespace."""
+        self.assertEqual(greet("   "), "Hello,    !")
+
 
 if __name__ == "__main__":
     unittest.main()
